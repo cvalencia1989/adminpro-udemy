69f219b50fcfc25571fc416b44b1c244c388d046
claudio_valencia <ing.cvalencia@gmail.com>
2020-04-18 08:30:30 -0400

    primer comit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// Rutas
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { NopagefoundComponent } from './nopagefound/nopagefound.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { Grarficas1Component } from './pages/grarficas1/grarficas1.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { SidebardComponent } from './shared/sidebard/sidebard.component';
+import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
+import { PagesComponent } from './pages/pages.component';
+import { RegisterComponent } from './login/register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    NopagefoundComponent,
+    DashboardComponent,
+    ProgressComponent,
+    Grarficas1Component,
+    HeaderComponent,
+    SidebardComponent,
+    BreadcrumbsComponent,
+    PagesComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pages/grarficas1/grarficas1.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-grarficas1',
+  templateUrl: './grarficas1.component.html',
+  styles: []
+})
+export class Grarficas1Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
